9282e258b3ecfd46ac890b6d62bd0b6ebbf74ad1
AlbrightSunguti <albright.sunguti@abxtractventurelabs.com>
2023-03-08 16:55:21 +0300

    chore: add firebase config options and database url

--- src/environments/firebase.config.ts
+++ src/environments/firebase.config.ts
@@ -1,8 +1,11 @@
 export const FireBaseConfig = {
-  apiKey: "<YOUR_API_KEY>",
-  authDomain: "<YOUR_AUTH_DOMAIN>",
-  databaseURL: "<YOUR_DATABASE_URL>",
-  projectId: "<YOUR_PROJECT_ID>",
-  storageBucket: "<YOUR_STORAGE_BUCKETS>",
-  messagingSenderId: "<YOUR_MESSAGING_SENDER_ID>",
+  apiKey: "AIzaSyDv0GwIDyTplf-1dO571RcmdIKxpdSPiYc",
+  authDomain: "ngrx001.firebaseapp.com",
+  projectId: "ngrx001",
+  storageBucket: "ngrx001.appspot.com",
+  messagingSenderId: "263489247370",
+  appId: "1:263489247370:web:fcc0e64271582b7f6e3573",
+  measurementId: "G-1T9KEMFNFF",
+  databaseUrl:
+    "https://console.firebase.google.com/project/ngrx001/database/ngrx001-default-rtdb/data/~2F",
 };
